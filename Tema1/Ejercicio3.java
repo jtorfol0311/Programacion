3852e9f8b92b8947bbfb6a362e29ea575dd645c6
jtorfol0311 <jtorfol0311@gmail.com>
2026-09-22 18:39:46 +0200

    Add files via upload

---
+++ Tema1/Ejercicio3.java
@@ -0,0 +1,7 @@
+public class Ejercicio3 {	
+  public static void main(String[] args) { 
+    System.out.println(" Nombre: Juan Manuel ");
+	System.out.println(" Direccion: c/pepe n7");
+	System.out.println(" Numero de telefono: 643864975");
+  }
+}
