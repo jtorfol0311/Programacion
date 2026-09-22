07b854ef2ba5e0ef57dda3946b9bbb6ad29e44d8
jtorfol0311 <jtorfol0311@gmail.com>
2026-09-22 16:21:03 +0000

    Restore Tema1 directory

---
+++ Tema1/Ejercicio3.java
@@ -0,0 +1,7 @@
+public class Ejercicio3 {	
+  public static void main(String[] args) { 
+    System.out.println(" Nombre: Juan Manuel ");
+	System.out.println(" Direccion: c/arnau n45 ");
+	System.out.println(" Telefono: 643123647 ");
+  }
+}
