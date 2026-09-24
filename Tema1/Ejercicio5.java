2598f5d16ef78ed5c7e78ed1760aeaa9bc7eb6e2
jtorfol0311 <jtorfol0311@gmail.com>
2026-09-24 09:10:23 +0200

    Add files via upload

---
+++ Tema1/Ejercicio5.java
@@ -0,0 +1,12 @@
+public class Ejercicio5 {	
+  public static void main(String[] args) { 
+    System.out.println(" Lunes\tMartes\tMiércoles\tJueves\tViernes");
+	System.out.println(" LMSGI\tBADAT\tSIINF\t\tPROGR\tENDES");
+	System.out.println(" LMSGI\tBADAT\tSIINF\t\tPROGR\tBADAT");
+	System.out.println(" SIINF\tSASP\tIPE1\t\tLMSGI\tBADAT");
+	System.out.println(" ----------------------------------------------");
+	System.out.println(" SIINF\tENDES\tIPE1\t\tSIINF\tIPE1");
+	System.out.println(" PROGR\tPROGR\tDASPGS\t\tBADAT\tPROGR");
+	System.out.println(" PROGR\tPROGR\tENDES\t\tBADAT\tPROGR");
+  }
+}
