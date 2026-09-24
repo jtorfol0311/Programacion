ec580b2a3bd65b486a52968147e0cb2c1829474d
jtorfol0311 <jtorfol0311@gmail.com>
2026-09-24 08:36:29 +0200

    Add files via upload

---
+++ Tema1/Ejercicio4.java
@@ -0,0 +1,15 @@
+public class Ejercicio4 {	
+  public static void main(String[] args) { 
+    System.out.println(" Inglés\tEspañol");
+	System.out.println(" Name\tNombre ");
+	System.out.println(" Hello\tHola");
+	System.out.println(" Blue\tAzul");
+	System.out.println(" Key\tLlave");
+	System.out.println(" Mouse\tRatón");
+	System.out.println(" Table\tMesa");
+	System.out.println(" Rock\tPiedra");
+	System.out.println(" River\tRio");
+	System.out.println(" Apple\tManzana");
+	System.out.println(" Sun\tSol");
+  } 
+}
